7bbf3380a4e3f51419e65ab814f33b801a5fcb6f
devslife7 <devslife7@gmail.com>
2025-05-05 20:24:07 -0400

    moved navbar and footer to layout folder

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,14 @@ import Hero from "../components/Hero"
 import KeyPoints from "@/components/KeyPoints"
 import Projects from "@/components/Projects"
 import ContactUs from "@/components/ContactUs"
-import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Hero />
       <KeyPoints />
       <Projects />
       <ContactUs />
-      <Footer />
     </div>
   )
 }
