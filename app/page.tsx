947513c7bc998b24bc0ee46faaa4bfc05c5b0ea0
devslife7 <devslife7@gmail.com>
2025-05-05 20:00:31 -0400

    added navbar to boiler plate

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import KeyPoints from "@/components/KeyPoints"
 import Projects from "@/components/Projects"
 import ContactUs from "@/components/ContactUs"
 import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Hero />
       <KeyPoints />
       <Projects />
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="h-10 bg-gray-600">
+      <div>Navbar</div>
+    </div>
+  )
+}
