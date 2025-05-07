14f99ef89fe9edfedbe1e782bfcc1c37acfdbd5f
devslife7 <devslife7@gmail.com>
2025-05-07 13:50:54 -0400

    added keypoints

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,36 @@
+import heroBackground from "@/public/img/hero-house.webp"
+import Image from "next/image"
+import Link from "next/link"
+import { buttonVariants } from "./ui/button"
+import { cn } from "@/lib/utils"
+
 export default function Hero() {
   return (
-    <div className="h-92 bg-red-400">
-      <div>hero</div>
+    <div className="h-[30rem] lg:mx-auto lg:h-[54rem]">
+      <div className="relative h-full w-full bg-black/30 lg:px-8">
+        <Image
+          alt="Hero Kitchen"
+          src={heroBackground}
+          fill
+          sizes="100vw"
+          priority
+          className="-z-10 object-cover"
+          placeholder="blur"
+        />
+        <div className="absolute my-container inset-0 mx-auto flex h-full w-full items-center justify-center text-center text-white lg:w-auto lg:justify-start lg:text-left">
+          <div className="py-14 lg:py-20">
+            <p className="font-roboto text-4xl font-semibold leading-tight md:text-5xl lg:text-7xl lg:leading-tight">
+              Remodeling Made Simple, <br /> Done Right
+            </p>
+            <p className="font-roboto leading-tight md:text-xl lg:text-2xl">
+              Remodeling Made Simple, <br /> Done Right
+            </p>
+            <Link href="/work" className={cn(buttonVariants(), "mt-4 font-light lg:text-lg")}>
+              Explore Gallery
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/KeyPoints.tsx
+++ components/KeyPoints.tsx
@@ -1,7 +1,37 @@
+import { UserCheck, UsersRound, Wrench } from "lucide-react"
+
+const points = [
+  {
+    icon: <UsersRound className="mx-auto" size={32} />,
+    title: "Expertise Team",
+    description: "Skilled professionals with years of industry experience.",
+  },
+  {
+    icon: <Wrench className="mx-auto" size={32} />,
+    title: "Quality & Craftmanship",
+    description: "Focus on durable materials and attention to detail.",
+  },
+  {
+    icon: <UserCheck className="mx-auto" size={32} />,
+    title: "Customer Satisfaction",
+    description: "Strong client reviews and repeat business.",
+  },
+]
+
 export default function KeyPoints() {
   return (
-    <div className="h-92 bg-blue-500">
-      <div>KeyPoints</div>
+    <div className="py-0 my-container lg:py-4 bg-gray-50 relative rounded-md top-[-100]">
+      <div className="flex flex-wrap justify-center gap-8 py-10 md:gap-0 lg:justify-evenly">
+        {points.map(point => {
+          return (
+            <div className="max-w-sm p-2 text-center">
+              <div className="text-primary ">{point.icon}</div>
+              <h1 className="mt-2 mb-2 text-xl font-medium md:text-xl">{point.title}</h1>
+              <p className="opacity-60 md:text-lg">{point.description}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
